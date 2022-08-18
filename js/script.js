21d646c9a5e3f12560c96423d5de95b1149d391d
Piotr <korotko.piotr@gmail.com>
2022-08-18 16:09:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,123 @@
+let todoInput
+let errorInfo
+let addBtn
+let ulList
+let newTodo
+
+let popup
+let popupInfo
+let todoToEdit
+let popupInput
+let popupAddBtn
+let popupCloseBtn
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTask)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+}
+
+const addNewTask = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		addTools()
+		ulList.append(newTodo)
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!'
+	}
+}
+
+const addTools = () => {
+	const divTools = document.createElement('div')
+	divTools.classList.add('tools')
+	newTodo.append(divTools)
+
+	const btnComplete = document.createElement('button')
+	btnComplete.classList.add('complete')
+	btnComplete.innerHTML = '<i class="fas fa-check"></i>'
+
+	const btnEdit = document.createElement('button')
+	btnEdit.classList.add('edit')
+	btnEdit.textContent = 'EDIT'
+
+	const btnDelete = document.createElement('button')
+	btnDelete.classList.add('delete')
+	btnDelete.innerHTML = '<i class="fas fa-times"></i>'
+
+	divTools.append(btnComplete, btnEdit, btnDelete)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = e => {
+	todoToEdit = e.target.closest('li')
+	popup.style.display = 'flex'
+	popupInput.value = todoToEdit.firstChild.textContent
+}
+
+const closePopup = () => {
+	popup.style.display = 'none'
+	popup.textContent = ''
+}
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popup.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz podać treść!'
+	}
+}
+
+const deleteTodo = (e) => {
+	e.target.closest('li').remove()
+
+	const allTodos = document.querySelectorAll('li')
+	console.log(allTodos.length);
+	if (allTodos.length === 0){
+		errorInfo.textContent = 'Brak zadań na liście.'
+	}
+}
+
+const enterKeyCheck = e => {
+	if(e.key === 'Enter'){
+		addNewTask()
+	}
+	
+}
+
+
+document.addEventListener('DOMContentLoaded', main)
